eb29eace91d5f8dc884404e0d1888b2323224fa2
thiyangt <tstalagala@gmail.com>
2024-08-18 11:31:02 +0530

    updated

---
+++ writing.R
@@ -0,0 +1,21 @@
+
+
+1. Specific issue  
+
+What factors are contributing
+to the increased rates of antibiotic
+resistent in Hospital X over the 
+past five years.
+
+Why it is important?
+  
+2. Difficulty in doing some task
+
+3. Contradiction
+
+Why do some studies show a strong 
+correlation between saturated fat 
+intake and heart disease, 
+while others do not
+
+
